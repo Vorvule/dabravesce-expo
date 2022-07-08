0c9b05a0ba9c045da00c3d2117fd79b33d9c6ec0
Alexander Vorvul <7105690@gmail.com>
2022-07-08 14:13:35 +0300

    Container View to fix html height scrolling added

--- App.js
+++ App.js
@@ -3,11 +3,20 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { BottomTabs } from "./tabs/BottomTabs";
+import { View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <BottomTabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <BottomTabs />
+      </NavigationContainer>
+    </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 2,
+  },
+});
