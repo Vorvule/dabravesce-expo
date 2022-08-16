a7468aacac7c29d51ddedcc06d4e6050c2e642fb
Alexander Vorvul <7105690@gmail.com>
2022-08-16 23:19:53 +0300

    TouchableOpacity with FontAwesome icon instead of Button introduced

--- screens/Core/ChapterSound.js
+++ screens/Core/ChapterSound.js
@@ -1,12 +1,12 @@
-import { Button, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
 import * as React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
 import { Audio } from "expo-av";
+import { TouchableOpacity } from "react-native-web";
 
 export default function ChapterSound({ chapterAudio }) {
-  console.log(chapterAudio);
-
   const [sound, setSound] = React.useState();
 
   async function playSound() {
@@ -18,22 +18,29 @@ export default function ChapterSound({ chapterAudio }) {
     await sound.playAsync();
   }
 
+  async function unloadSound() {
+    sound.unloadAsync();
+  }
+
   React.useEffect(() => {
-    return sound
-      ? () => {
-          sound.unloadAsync();
-        }
-      : undefined;
+    return sound ? unloadSound : undefined;
   }, [sound]);
 
-  return <Button title="Слухаць" onPress={playSound} />;
+  return (
+    <TouchableOpacity style={styles.touchable} onPress={playSound}>
+      <FontAwesome name="play" size={15} color={"teal"} />
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-  view: {
-    width: "100%",
-    maxWidth: 800,
-    alignSelf: "center",
-    padding: 16,
+  touchable: {
+    width: 40,
+    height: 40,
+    borderRadius: "50%",
+    borderColor: "teal",
+    borderWidth: 2,
+    paddingLeft: 14,
+    paddingTop: 10,
   },
 });
