830a278337ad330fef7b386937cb723272ab76ef
Alexander Vorvul <7105690@gmail.com>
2022-08-16 22:28:56 +0300

    expo-av and @react-navigation/native added; { Autio } to Core.js added

--- screens/Core/Core.js
+++ screens/Core/Core.js
@@ -1,12 +1,38 @@
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
+import * as React from "react";
+
+import { Audio } from "expo-av";
 
 import ChapterName from "./ChapterName";
 import ChapterText from "./ChapterText";
 
 export default function Core({ chapter }) {
+  console.log(chapter.audio);
+
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../../assets/01.mp3")
+    );
+
+    setSound(sound);
+    await sound.playAsync();
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.view}>
       <ChapterName chapterName={chapter.name} />
+      <Button title="Play Sound" onPress={playSound} />
       <ChapterText chapterText={chapter.text} />
     </View>
   );
