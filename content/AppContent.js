0c73d45de8aef772dc2068e934efbd5a5e5d34a2
Alexander Vorvul <7105690@gmail.com>
2022-11-10 15:21:49 +0300

    minHeight detection adjusted

--- content/AppContent.js
+++ content/AppContent.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 
-import { View, useWindowDimensions } from "react-native";
+import { View } from "react-native";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 
 import { BottomTabs } from "./BottomTabs";
 import { styles } from "../styles/styles";
+
+import { minHeight } from "../functions/device";
 
 export default function AppContent() {
   const whiteTheme = {
@@ -17,10 +19,8 @@ export default function AppContent() {
     },
   };
 
-  const { height } = useWindowDimensions(); // for the sake of mobile browser; if fails, delete
-
   return (
-    <View style={[styles.appContainer, { height: height }]}>
+    <View style={[styles.appContainer, { minHeight: minHeight() }]}>
       <StatusBar backgroundColor="teal" style="light" />
 
       <NavigationContainer theme={whiteTheme}>
--- functions/device.js
+++ functions/device.js
@@ -1,5 +1,24 @@
-export function sizeFont(deviceWidth, deviceHeight, mobileSize, desktopSize) {
-  const deviceIsMobile = deviceWidth < 900 || deviceHeight < 900;
+import { Platform, useWindowDimensions } from "react-native";
 
-  return deviceIsMobile ? { fontSize: mobileSize } : { fontSize: desktopSize };
+export function sizeFont(width, height, mobileFontSize, desktopFontSize) {
+  const sizedFont = deviceIsMobile(width, height)
+    ? { fontSize: mobileFontSize }
+    : { fontSize: desktopFontSize };
+
+  return sizedFont;
+}
+
+export function deviceIsMobile() {
+  const { width, height } = useWindowDimensions();
+
+  return (width < 900 || height < 900);
+}
+
+export function minHeight() {
+  const { height } = useWindowDimensions();
+
+  const minHeight =
+    Platform.OS == "web" && deviceIsMobile() ? height + 80 : height - 4;
+
+  return minHeight;
 }
