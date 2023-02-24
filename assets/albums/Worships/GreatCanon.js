e2de296c01fa3dfd48535462f223972eb1b7ab5d
Alexander Vorvul <7105690@gmail.com>
2023-02-24 23:52:36 +0300

    great canon split into four days

---
+++ assets/albums/Worships/GreatCanon.js
@@ -0,0 +1,27 @@
+import { Monday } from "./GreatCanon/Monday";
+import { Tuesday } from "./GreatCanon/Tuesday";
+import { Wednesday } from "./GreatCanon/Wednesday";
+import { Thursday } from "./GreatCanon/Thursday";
+
+export const GreatCanon = [
+  {
+    name: "У панядзелак першага тыдня вялікага посту на вялікім павячэр’і",
+    idEn: "Monday",
+    text: Monday,
+  },
+  {
+    name: "У аўторак першага тыдня Вялікага посту на вялікім павячэр’і",
+    idEn: "Tuesday",
+    text: Tuesday,
+  },
+  {
+    name: "У сераду першага тыдня Вялікага посту на вялікім павячэр’і",
+    idEn: "Wednesday",
+    text: Wednesday,
+  },
+  {
+    name: "У чацвер першага тыдня Вялікага посту на вялікім павячэр’і",
+    idEn: "Thursday",
+    text: Thursday,
+  },
+];
--- assets/albums/Worships/worships.js
+++ assets/albums/Worships/worships.js
@@ -1,29 +1,31 @@
+import { GreatCanon } from "./GreatCanon";
 import { Acathisti } from "./Acathisti";
 import { Canons } from "./Canons";
 import { Liturgy } from "./Liturgy";
 
 export const worships = {
   name: "Богаслужэнні",
-  idBe: "Богаслужэнні",
   idEn: "Worship",
   text: [
     {
-      name: "Літургія",
-      idBe: "Літургія",
-      idEn: "Liturgy",
-      text: Liturgy,
+      name: "Акафісты",
+      idEn: "Acathisti",
+      text: Acathisti,
+    },
+    {
+      name: "Вялікі канон прападобнага айца нашага Андрея Крыцкага і Іерусалімскага",
+      idEn: "Great Canon",
+      text: GreatCanon,
     },
     {
       name: "Каноны",
-      idBe: "Каноны",
       idEn: "Canons",
       text: Canons,
     },
     {
-      name: "Акафісты",
-      idBe: "Акафісты",
-      idEn: "Acathisti",
-      text: Acathisti,
+      name: "Літургія",
+      idEn: "Liturgy",
+      text: Liturgy,
     },
   ],
 };
